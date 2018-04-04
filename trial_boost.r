6eb0c86819e58a68927c55295bfad1454590d216
Zehao <36336813+zehaosu@users.noreply.github.com>
2018-04-04 17:24:01 +0800

    Update trial_boost.r

--- trial_boost.r
+++ trial_boost.r
@@ -7,12 +7,27 @@ for(i in 1:length(url)) download.file(url[i], name[i])
 x_train <- read.csv("./x_train.csv", header=TRUE)
 y_train <- read.csv("./y_train.csv", header=TRUE)
 x_test <- read.csv("./x_test.csv", header=TRUE)
-par(mfrow=c(2,2))
-for (i in 2:18){
-  hist(x_train[, i], main=colnames(x_train)[i])
-}
-# Boosting
+x_train_mat <- model.matrix(~.-1, x_train[, -1])
+
+# EDA
+corr <- cor(x_train)
+library(ggcorrplot)
+ggcorrplot(corr, colors=c("darkblue", "white", "darkred"))
+
+# boosting
 library(xgboost)
-bst <- xgboost(data=as.matrix(x_train[,2:ncol(x_train)]), label=as.matrix(y_train[,2]), 
-               max.depth=2, eta=0.1, nthread=2, nround=10, objective="binary:logistic")
-bst_pred <- predict(bst, as.matrix(x_train[,2:ncol(x_train)]))
+bst <- xgboost(data=x_train_mat, label=as.matrix(y_train[,2]), 
+               max.depth=2, eta=0.15, nthread=2, nround=50, objective="binary:logistic")
+bst_pred <- predict(bst, x_train_mat)
+
+# logistic regression
+library(glmnet)
+logit <- cv.glmnet(x_train_mat, as.matrix(as.factor(y_train[,-1])),
+                family="binomial", alpha=0.1, nfolds=5)
+logit_pred <- predict.cv.glmnet(logit, newx=x_train_mat, type="response", s="lambda.min")
+library(pROC)
+roc_logit <- roc(y_train[, 2]~logit_pred)
+plot(roc_logit, legacy.axes=TRUE)
+lines(1-roc_logit$specificities, roc_logit$thresholds, lty=2)
+legend("bottomright", legend=c(paste("Logistic AUC ", round(roc_logit$auc, 4), sep=""),
+                               "Threshold"), col=1, lty=1:2)
