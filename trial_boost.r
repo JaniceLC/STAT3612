3be40caadcb11f29b7153a927b75c2775bb0c1e5
Zehao <36336813+zehaosu@users.noreply.github.com>
2018-04-05 12:50:02 +0800

    Update trial_boost.r

--- trial_boost.r
+++ trial_boost.r
@@ -8,12 +8,15 @@ x_train <- read.csv("./x_train.csv", header=TRUE)
 y_train <- read.csv("./y_train.csv", header=TRUE)
 x_test <- read.csv("./x_test.csv", header=TRUE)
 
-# design *matrix* (excl. labels)
+# design matrix
 x_train_mat <- model.matrix(~.-1, x_train[, -1])
-
-# *data.frame* of train data (incl. labels)
+# matrix of train data
+train_mat <- cbind(x_train_mat, y_train[, 2])
+colnames(train_mat)[ncol(train_mat)] <- "FlagAIB"
+# data frame of train data
 train <- data.frame(x_train_mat)
 train$FlagAIB <- factor(y_train[, 2])
+train_reduced <- train[sample(1:nrow(train), floor(nrow(train)/2)), ]
 
 # EDA
 corr <- cor(x_train_mat)
@@ -23,7 +26,18 @@ ggcorrplot(corr,type="lower", outline.col="white",
 ggcorrplot(corr,type="lower", outline.col="white",
            insig="blank", method="circle")
 
-# loading packages
+# t-sne
+cols <- c("pink", "skyblue")
+library(tsne)
+ecb = function(x, y){
+  plot(x, t='n')
+  text(x, labels=train_mat[,24], col=cols[train_mat[,24] +1])
+}
+
+tsne_res = tsne(train_mat[,1:23], epoch_callback = ecb, 
+                perplexity=50, epoch=50)
+
+# preprocessing
 library(caret)
 library(dplyr)
 
@@ -63,13 +77,34 @@ predict(gbm, newdata=data.frame(model.matrix(~.-1, x_test[, -1])),
 trellis.par.set(caretTheme())
 densityplot(gbm, pch="|")
 
-# support vector machine
-svm <- train(FlagAIB~., data=train,
+## support vector machine
+## high complexity
+levels(train_reduced$FlagAIB) <- c("worse", "better")
+svm <- train(FlagAIB~., data=train_reduced,
              method="svmRadial",
              trControl=fitControl,
              preProc=c("center", "scale"),
-             tuneLength=8,
+             tuneLength=2,
              metric="ROC")
+
+## extreme gradient boosting
+xgb <- train(FlagAIB~., data=train,
+             method="xgbTree",
+             trControl=fitControl,
+             preProc=c("center", "scale"),
+             metric="ROC")
+trellis.par.set(caretTheme())
+densityplot(xgb, pch="|")
+
+# glm elastic net
+lgt <- train(FlagAIB~., data=train, 
+             method = "glmnet", 
+             trControl = fitControl,
+             metric = "ROC",
+             tuneGrid = expand.grid(alpha = 1,
+                                    lambda = seq(0.001,0.1,by = 0.001)))
+trellis.par.set(caretTheme())
+densityplot(lgt, pch="|")
 
 
 # boosting
