5c2fb457e92552b38f3728a931c5f4de0c288d77
Zehao <36336813+zehaosu@users.noreply.github.com>
2018-04-11 12:06:27 +0800

    Update xgb.R

--- R/xgb.R
+++ R/xgb.R
@@ -2,15 +2,19 @@
 # XGBoost #
 ###########
 
+if(!require(xgboost)) install.packages("xgboost")
+if(!require(ggplot2)) install.packages("ggplot2")
+
 library(xgboost)
 library(Matrix)
 library(ggplot2)
 
-# construct DMatrix for XGBoost
-#dtrain <- xgb.DMatrix(data=sparse.model.matrix(~.-1, data=train.x.bin),
-#                      label=as.numeric(train.y$FlagAIB)-1)
+# construct DMatrix for XGBoost (optional)
+# dtrain <- xgb.DMatrix(data=sparse.model.matrix(~.-1, data=train.x.bin),
+#                       label=as.numeric(train.y$FlagAIB)-1)
 
 # parameter tuning
+# package Caret also provides a systematic framework for tuning
 paramGrid <- expand.grid(
   eta=c(0.02),
   max_depth=c(6),
@@ -29,6 +33,10 @@ for (i in 1:nrow(paramGrid)){
     nround=1000,
     verbose=0,
     nfold=5,
+    # ---XGBoost documentation---
+    # validation error needs to decrease at least every
+    # early_stopping_rounds to continue training
+    early_stopping_rounds=50,
     eval_metric="auc",
     objective="binary:logistic",
     booster="gbtree"
@@ -45,17 +53,12 @@ for (i in 1:nrow(paramGrid)){
 }
 
 # train model based on tuned result
-param <- list(
-  eta=0.02,
-  max_depth=6,
-  subsample=0.55,
-  colsample_bytree=0.7
-)
+param <- best_param
 xgb <- xgboost(
   data=as.matrix(train.x.bin),
   label=as.matrix(train.y$FlagAIB), 
-  params=param,
-  nround=670,
+  params=best_param,
+  nround=best_round,
   verbose=0,
   nfold=5,
   eval_metric="auc",
@@ -63,7 +66,15 @@ xgb <- xgboost(
   booster="gbtree"
 )
 
-# plot importance of variable
+# ---last used tuned parameters---
+# param <- list(
+#   eta=0.02,
+#   max_depth=6,
+#   subsample=0.55,
+#   colsample_bytree=0.7
+# )
+
+# plot variable importance
 imp.matrix <- xgb.importance(feature_names=colnames(train.x.bin), model=xgb)
 xgb.ggplot.importance(imp.matrix)
 xgb.plot.importance(imp.matrix)
@@ -71,4 +82,47 @@ xgb.plot.importance(imp.matrix)
 # predict
 pred.y <- predict(xgb, newdata=as.matrix(test.x.bin))
 test.y <- data.frame(StudentID=1:length(pred.y), FlagAIB=pred.y)
-write.csv(test.y, file="~/y_test_2_2.csv", row.names=FALSE)
+write.csv(test.y, file="~/y_test_2.csv", row.names=FALSE)
+
+# DART (Dropout Additive Regression Trees)
+# inherits gbtree and has additional parameters
+paramGrid <- expand.grid(
+  eta=c(0.02),
+  max_depth=c(6),
+  subsample=0.55,
+  colsample_bytree=0.7, # randomForest
+  rate_drop=c(0.8),
+  skip_drop=c(0.35)
+)
+best_param <- list()
+best_auc <- 0
+best_round <- 0
+for (i in 1:nrow(paramGrid)){
+  current_param <- as.list(paramGrid[i,])
+  history <- xgb.cv(
+    data=as.matrix(train.x.bin),
+    label=as.matrix(train.y$FlagAIB),
+    params=current_param,
+    nround=1000,
+    verbose=0,
+    nfold=5,
+    # ---XGBoost documentation---
+    # validation error needs to decrease at least every
+    # early_stopping_rounds to continue training
+    early_stopping_rounds=50,
+    eval_metric="auc",
+    objective="binary:logistic",
+    booster="gbtree",
+    sample_type="uniform",
+    normalize_type="tree"
+  )
+  current_round <- history$best_iteration
+  current_auc <- history$evaluation_log$test_auc_mean[current_round]
+  if(current_auc > best_auc){
+    best_param <- current_param
+    best_auc <- current_auc
+    best_round <- current_round
+  }
+  # make verbose
+  print(paste("Round ", i, " completed", sep=""))
+}
