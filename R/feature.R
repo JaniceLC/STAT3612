c372dbf23262c6e8e7a85a2f035ae36522e82a32
Zehao <36336813+zehaosu@users.noreply.github.com>
2018-05-01 16:38:59 +0800

    Add files via upload

--- R/feature.R
+++ R/feature.R
@@ -1,5 +1,5 @@
 #####################
-# Data manipulation #
+# DATA MANIPULATION #
 #####################
 
 if(!require(dplyr)) install.packages("dplyr")
@@ -20,7 +20,11 @@ train.x <- read.csv(url[1], header=TRUE, row.names=1)
 train.y <- read.csv(url[2], header=TRUE, row.names=1)
 test.x <- read.csv(url[3], header=TRUE, row.names=1)
 
-val.index <- sample(c(1:nrow(train.x)), size =floor(0.2*nrow(train.x)), replace=FALSE)
+# set randomization seed
+set.seed(2018)
+
+# separate a validation set of 20% of original data
+val.index <- sample(c(1:nrow(train.x)), size=floor(0.2*nrow(train.x)), replace=FALSE)
 val.x <- train.x[val.index,]
 val.y <- train.y[val.index,]
 train.x <- train.x[-val.index,]
@@ -38,6 +42,7 @@ recast.type <- function(df){
   df$EdFather[df$EdFather==8] <- 3
   return(df)
 }
+
 # merging regions
 add.didactic <- function(df){
   df <- df %>% 
@@ -48,86 +53,93 @@ add.didactic <- function(df){
   return(df)
 }
 
-impute.value <- function(df){
-  asia.mother.index <- intersect(which(df$Didactic=="ASIA"), which(is.na(df$EdMother)))
-  nasia.mother.index <- intersect(which(df$Didactic=="NASIA"), which(is.na(df$EdMother)))
-  sgp.mother.index <- intersect(which(df$Didactic=="SGP"), which(is.na(df$EdMother)))
-  asia.father.index <- which(df$Didactic=="ASIA" && is.na(df$EdFather))
-  nasia.father.index <- which(df$Didactic=="NASIA" && is.na(df$EdFather))
-  sgp.father.index <- which(df$Didactic=="SGP" && is.na(df$EdFather))
-  asia.mother.fill <- which.max(table(df$EdMother))[[1]]
-  nasia.mother.fill <- which.max(table(df$EdMother))[[1]]
-  sgp.mother.fill <- which.max(table(df$EdMother))[[1]]
-  asia.father.fill <- which.max(table(df$EdFather))[[1]]
-  nasia.father.fill <- which.max(table(df$EdFather))[[1]]
-  sgp.father.fill <- which.max(table(df$EdFather))[[1]]
-  imputed=c(asia.mother.fill, nasia.mother.fill, sgp.mother.fill,
-            asia.father.fill, nasia.father.fill, sgp.father.fill)
-  return(imputed)
+income.women <- data.frame(
+  EdMother=rep(1:7, 6),
+  Region=rep(c("USA", "ENG", "HKG", "JPN", "TWN", "SGP"), each=7),
+  InMother=c(c(77, 77, 100, 100, 134, 165, 205),
+    c(61, 61, 100, 100, 125, 168, 234),
+    rep(c(80, 80, 100, 100, 126, 154, 215), 4))
+)
+
+income.men <- data.frame(
+  EdFather=rep(1:7, 6),
+  Region=rep(c("USA", "ENG", "HKG", "JPN", "TWN", "SGP"), each=7),
+  InFather=c(c(68, 68, 100, 100, 113, 171, 244),
+             c(72, 72, 100, 100, 124, 148, 174),
+             rep(c(75, 75, 100, 100, 109, 135, 170), 4))
+)
+
+add.income <- function(df){
+  df <- df %>% 
+    left_join(income.women) %>%
+    left_join(income.men) %>%
+    mutate(Income=InMother+InFather) %>%
+    as.data.frame()
+  return(df)
 }
 
-# impute by the most frequent value in each group
-impute.ed <- function(df, imputed){
-  asia.mother.index <- intersect(which(df$Didactic=="ASIA"), which(is.na(df$EdMother)))
-  nasia.mother.index <- intersect(which(df$Didactic=="NASIA"), which(is.na(df$EdMother)))
-  sgp.mother.index <- intersect(which(df$Didactic=="SGP"), which(is.na(df$EdMother)))
-  asia.father.index <- which(df$Didactic=="ASIA" && is.na(df$EdFather))
-  nasia.father.index <- which(df$Didactic=="NASIA" && is.na(df$EdFather))
-  sgp.father.index <- which(df$Didactic=="SGP" && is.na(df$EdFather))
-  df$EdMother[asia.mother.index] <- imputed[1]
-  df$EdMother[nasia.mother.index] <- imputed[2]
-  df$EdMother[sgp.mother.index] <- imputed[3]
-  df$EdFather[asia.father.index] <- imputed[4]
-  df$EdFather[nasia.father.index] <- imputed[5]
-  df$EdFather[sgp.father.index] <- imputed[6]
+schooling.women <- data.frame(
+  EdMother=c(1:7),
+  ScMother=c(6, 9, 12, 13.5, 14.5, 16, 20.5)
+)
+
+schooling.men <- data.frame(
+  EdFather=c(1:7),
+  ScFather=c(6, 9, 12, 13.5, 14.5, 16, 20.5)
+)
+
+add.schooling <- function(df){
+  df <- df %>%
+    left_join(schooling.women) %>%
+    left_join(schooling.men) %>%
+    as.data.frame()
   return(df)
 }
 
 # set up recipe
-make.data <- function(df, imputed){
+make.data <- function(df){
   df <- df %>% 
     recast.type() %>% 
-    add.didactic() 
-   # impute.ed(imputed)
+    add.didactic() %>%
+    add.income() %>%
+    add.schooling() %>%
+    select(-Region-EdMother-EdFather-InMother-InFather)
   
-  rcp <- recipe(~.-Region, data=df) %>%
+  df <- recipe(~.-Region, data=df) %>%
     step_center(all_numeric()) %>%
     step_scale(all_numeric()) %>%
-    # step_ns(all_numeric(), df = 3) %>%
-    step_interact(terms = ~contains("EdMother"):contains("EdFather"), sep ="x") %>%
     step_dummy(Gender) %>%
     step_dummy(Didactic) %>%
-    step_interact(terms=~contains("Didactic"):starts_with("Ed")+ 
+    step_interact(terms=~contains("Didactic"):starts_with("Sc")+ 
                     contains("Didactic"):starts_with("ExMotif")+
                     contains("Didactic"):starts_with("InMotif")+
-                    contains("Didactic"):contains("Gender")) %>%
-    prep(training=df)
-  df <- bake(rcp, newdata=df)
-  df$Region <- NULL
+                    contains("Didactic"):contains("Gender")+
+                    contains("Didactic"):contains("Income")) %>%
+    prep(training=df) %>%
+    bake(newdata=df)
   return(df)
 }
 
 #####################################
 #####################################
 
-#imputed <- train.x %>% recast.type() %>% add.didactic() %>% impute.value()
-train.x <- make.data(train.x, imputed)
-
+# data treatment
+train.x <- make.data(train.x)
 train = cbind(FlagAIB = train.y, train.x)
-val.x <- make.data(val.x, imputed)
-test.x <- make.data(test.x, imputed)
+val.x <- make.data(val.x)
+test.x <- make.data(test.x)
 
 
 
-#########Remove Outlier from train.x 
+# remove outliers from train.x
 # quantify through logistic regression
 model <- glm(FlagAIB~., data=train, family="binomial")
-# cooks.dist <- cooks.distance(model, infl=influence(model, do.coef=TRUE))
 cooks.dist <- cooks.distance(model)
-cutoff=quantile(cooks.dist, prob=0.995)
-plot(cooks.dist, pch=19, cex=0.5)
-abline(h=cutoff, col="red", lwd=2)
+cutoff=quantile(cooks.dist, prob=0.99)
 train = train[which(cooks.dist<cutoff),]
 train.x <- train[,-1]
 train.y <- train[,1]
 
+# plot cooks distance and cutoff
+plot(cooks.dist, pch=19, cex=0.5)
+abline(h=cutoff, col="red", lwd=2)
